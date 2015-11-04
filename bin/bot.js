91aa309e1b3a108d29dfc417375f042ce79e800e
Sam Julien <Sam.Julien@energytrust.org>
2015-11-03 17:55:08 -0800

    Initial commit

---
+++ bin/bot.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var BuddyBot = require('../lib/buddybot');
+
+var token = process.env.BOT_API_KEY;
+var name = process.env.BOT_NAME;
+
+var buddybot = new BuddyBot({
+	token: token,
+	name: name
+});
+
+buddybot.run();
